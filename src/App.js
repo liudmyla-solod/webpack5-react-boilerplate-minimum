ff08fe269bed9f0f2b1b70ab16a49f6313b197e3
Solod Liudmyla <solodludmila@gmail.com>
2021-08-26 18:10:20 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Webpack-5 boilerplate for React.
+            </div>
+        );
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+};
